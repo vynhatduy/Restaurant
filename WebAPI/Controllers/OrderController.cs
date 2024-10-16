280c60e5f17dd73ec22d5dde5ab8a39d08d81c16
vynhatduy <vynhatduy111@gmail.com>
2024-10-16 15:46:38 +0700

    Add order controller and table controller

--- WebAPI/Controllers/OrderController.cs
+++ WebAPI/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NhaHang.Models.Response;
 using WebAPI.Helpers;
@@ -120,6 +121,7 @@ namespace WebAPI.Controllers
             }
         }
         [HttpGet("OrderDetailByOrderId/{IdHoaDon:guid}")]
+
         public IActionResult GetOrderDetailByOrderId(Guid IdHoaDon)
         {
             try
@@ -149,22 +151,67 @@ namespace WebAPI.Controllers
             }
         }
         [HttpPost("Order/Create/{IdNhanVien:guid}")]
+        [Authorize(Roles = "Administrator,Management,User")]
         public IActionResult CreateOrderByEmployee(Guid IdNhanVien, OrderRequestModel model)
+        {
+            try
+            {
+                var nhanvien = _context.NhanViens.FirstOrDefault(x => x.IdNhanVien == IdNhanVien);
+                if (nhanvien != null)
+                {
+                    var item = _context.HoaDons.Where(x => x.IdBan == model.IdBan).OrderByDescending(x => x.NgayCapNhat).FirstOrDefault();
+                    if (item != null)
+                    {
+                        if (item.TrangThai == true && item.DaThanhToan == true)
+                        {
+                            var newGuid = Guid.NewGuid();
+                            var addOrder = Order.AddOrder(newGuid, IdNhanVien, model, _context);
+                            if (addOrder)
+                            {
+                                return Ok(new { Message = $"Tạo đơn hàng mới thành công với Id là : {newGuid}" });
+                            }
+                            return BadRequest(new { ErrorMessage = $"Xảy ra lỗi không xác định" });
+                        }
+                        else
+                        {
+                            return BadRequest(new { ErrorMessage = $"Bàn hiện tại không khả dụng" });
+                        }
+                    }
+                    else
+                    {
+                        var newGuid = Guid.NewGuid();
+                        var addOrder = Order.AddOrder(newGuid, IdNhanVien, model, _context);
+                        if (addOrder)
+                        {
+                            return Ok(new { Message = $"Tạo đơn hàng mới thành công với Id là : {newGuid}" });
+                        }
+                        return BadRequest(new { ErrorMessage = $"Xảy ra lỗi không xác định" });
+                    }
+                }
+                return BadRequest(new { Message = "Không có nhân viên nào như trên" });
+            }
+            catch(Exception e)
+            {
+                return BadRequest(new { ErrorMessage = $"Lỗi: {e.Message}" });
+            }
+        }
+        [HttpPost("Order/Create")]
+        public IActionResult CreateOrderByCustomer(OrderRequestModel model)
         {
             try
             {
                 var item = _context.HoaDons.Where(x => x.IdBan == model.IdBan).OrderByDescending(x => x.NgayCapNhat).FirstOrDefault();
                 if (item != null)
                 {
-                    if(item.TrangThai == true && item.DaThanhToan == true)
+                    if (item.TrangThai == true && item.DaThanhToan == true)
                     {
                         var newGuid = Guid.NewGuid();
-                        var addOrder =Order.AddOrder(newGuid, IdNhanVien, model, _context);
+                        var addOrder = Order.AddOrder(IdHoaDon:newGuid, IdNhanVien:Guid.Empty ,model:model,context:_context);
                         if (addOrder)
                         {
-                            return Ok(new { Message = $"Tạo đơn hàng mới thành công với Id là : {newGuid}" });
+                            return Ok(new { Message = $"Đơn hàng của bạn đã được tạo vui lòng chờ nhân viên xác nhận : {newGuid}" });
                         }
-                        return BadRequest(new { ErrorMessage = $"Xảy ra lỗi không xác định" });
+                        return BadRequest(new { ErrorMessage = $"Xảy ra lỗi không xác định vui lòng liên hệ nhân viên" });
                     }
                     else
                     {
@@ -174,19 +221,171 @@ namespace WebAPI.Controllers
                 else
                 {
                     var newGuid = Guid.NewGuid();
-                    var addOrder = Order.AddOrder(newGuid, IdNhanVien, model, _context);
+                    var addOrder = Order.AddOrder(IdHoaDon: newGuid, IdNhanVien: Guid.Empty, model: model, context: _context);
                     if (addOrder)
                     {
-                        return Ok(new { Message = $"Tạo đơn hàng mới thành công với Id là : {newGuid}" });
+                        return Ok(new { Message = $"Đơn hàng của bạn đã được tạo vui lòng chờ nhân viên xác nhận : {newGuid}" });
                     }
-                    return BadRequest(new { ErrorMessage = $"Xảy ra lỗi không xác định" });
+                    return BadRequest(new { ErrorMessage = $"Xảy ra lỗi không xác định vui lòng liên hệ nhân viên" });
                 }
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 return BadRequest(new { ErrorMessage = $"Lỗi: {e.Message}" });
             }
         }
-       
+        [HttpPut("Order/Update/{IdBan:guid}/{IdNhanVien:guid}")]
+        [Authorize(Roles ="Administrator,Management,User")]
+        public IActionResult UpdateOrder(Guid IdBan,Guid IdNhanVien)
+        {
+            try
+            {
+                var nhanvien = _context.NhanViens.FirstOrDefault(x => x.IdNhanVien == IdNhanVien);
+                if(nhanvien != null)
+                {
+                    var item = _context.HoaDons.Where(x => x.IdBan == IdBan).OrderByDescending(x => x.NgayCapNhat).FirstOrDefault();
+                    if (item != null)
+                    {
+                        if (item.TrangThai == false)
+                        {
+                            item.TrangThai = true;
+                            item.IdNhanVien = IdNhanVien;
+                            item.NgayCapNhat = DateTime.Now;
+                            _context.HoaDons.Update(item);
+                            _context.SaveChanges();
+                            return Ok($"Nhân viên: {nhanvien.HoTen} đã cập nhật đơn hàng");
+                        }
+                        else
+                        {
+                            return NotFound(new { Message = "Đơn hàng mới nhất đã được cập nhật" });
+                        }
+                    }
+                    return NotFound(new { Message = "Bàn này chưa có hóa đơn nào" });
+                }
+                else
+                {
+                    return BadRequest(new { Message = "Không có nhân viên nào như trên" });
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { ErrorMessage = $"Lỗi: {e.Message}" });
+            }
+        }
+        [HttpPut("OrderDetail/Update/{IdHoaDon:guid}/{IdNhanVien:guid}")]
+        [Authorize(Roles = "Administrator,Management,User")]
+        public IActionResult UpdateOrderDetail(Guid IdHoaDon, Guid IdNhanVien, List<Guid> DsIdSanPham)
+        {
+            try
+            {
+                var nhanvien = _context.NhanViens.FirstOrDefault(x => x.IdNhanVien == IdNhanVien);
+                if (nhanvien == null)
+                {
+                    return BadRequest(new { Message = "Không có nhân viên nào như trên" });
+                }
+
+                var hoaDon = _context.HoaDons.FirstOrDefault(x => x.IdHoaDon == IdHoaDon);
+                if (hoaDon == null)
+                {
+                    return NotFound(new { Message = "Hóa đơn không tồn tại" });
+                }
+
+                if (hoaDon.TrangThai == false)
+                {
+                    return BadRequest(new { Message = "Đơn hàng đang chờ xác nhận" });
+                }
+
+                if (DsIdSanPham.Count > 0)
+                {
+                    var chiTietHoaDons = _context.ChiTietHoaDons
+                    .Where(cthd => cthd.IdHoaDon == IdHoaDon && DsIdSanPham.Contains(cthd.IdSanPham.Value))
+                    .ToList();
+
+                    foreach (var chiTiet in chiTietHoaDons)
+                    {
+                        if (chiTiet.TrangThai == false)
+                        {
+                            chiTiet.TrangThai = true;
+                        }
+                    }
+
+                    _context.SaveChanges();
+
+                    return Ok(new { Message = "Cập nhật trạng thái thành công" });
+                }
+                else
+                {
+                    return BadRequest(new { Message = "Khong có id sản phẩm nào được truyền nvaof" });
+                }
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { ErrorMessage = $"Lỗi: {e.Message}" });
+            }
+        }
+
+        [HttpDelete("Order/Delete/{IdHoaDon:guid}/{IdNhanVien:guid}")]
+        [Authorize(Roles = "Administrator,Management")]
+        public IActionResult DeleteOrder(Guid IdHoaDon, Guid IdNhanVien)
+        {
+            try
+            {
+                var nhanvien = _context.NhanViens.FirstOrDefault(x => x.IdNhanVien == IdNhanVien);
+                if (nhanvien != null)
+                {
+                    var item = _context.HoaDons.Where(x => x.IdHoaDon == IdHoaDon).FirstOrDefault();
+                    if (item != null)
+                    {
+                        var result = Order.DeleteOrder(IdHoaDon: IdHoaDon, context: _context);
+                        if (result == true)
+                        {
+                            return Ok(new { Message = $"Nhân viên {nhanvien.HoTen} đã xóa thành công đơn hàng {IdHoaDon}" });
+                        }
+                        return BadRequest(new {Message= "Xóa không thành công vui lòng kiểm tra lại log"});
+                    }
+                    return NotFound(new { Message = "Không tìm thấy hóa đơn yêu cầu" });
+                }
+                else
+                {
+                    return BadRequest(new { Message = "Không có nhân viên nào như trên" });
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { ErrorMessage = $"Lỗi: {e.Message}" });
+            }
+        }
+        [HttpDelete("OrderDetail/Delete/{IdHoaDon:guid}/{IdNhanVien:guid}/{IdSanPham:guid}")]
+        [Authorize(Roles = "Administrator,Management")]
+        public IActionResult DeleteOrderDetail(Guid IdHoaDon, Guid IdNhanVien,Guid IdSanPham)
+        {
+            try
+            {
+                var nhanvien = _context.NhanViens.FirstOrDefault(x => x.IdNhanVien == IdNhanVien);
+                if (nhanvien != null)
+                {
+                    var item = _context.HoaDons.Where(x => x.IdHoaDon == IdHoaDon).FirstOrDefault();
+                    if (item != null)
+                    {
+                        var result = Order.DeleteOrderDetail(IdHoaDon: IdHoaDon,IdSanPham:IdSanPham, context: _context);
+                        if (result == true)
+                        {
+                            return Ok(new { Message = $"Nhân viên {nhanvien.HoTen} đã xóa thành công sản phẩm {IdSanPham} trong chi tiết đơn hàng {IdHoaDon}" });
+                        }
+                        return BadRequest(new {Message= "Xóa không thành công vui lòng kiểm tra lại log"});
+                    }
+                    return NotFound(new { Message = "Bàn này chưa có hóa đơn nào" });
+                }
+                else
+                {
+                    return BadRequest(new { Message = "Không có nhân viên nào như trên" });
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { ErrorMessage = $"Lỗi: {e.Message}" });
+            }
+        }
     }
 }
--- WebAPI/Helpers/Order.cs
+++ WebAPI/Helpers/Order.cs
@@ -10,7 +10,6 @@ namespace WebAPI.Helpers
         {
             try
             {
-
                 decimal? thanhTien = 0;
                 foreach (var item in model.DsChiTietHoaDon)
                 {
@@ -23,7 +22,6 @@ namespace WebAPI.Helpers
                     ChietKhau = "5%",
                     Coupon = model.Coupon,
                     DaThanhToan = model.DaThanhToan,
-                    IdNhanVien = IdNhanVien,
                     IdKhachHang = model.IdKhachHang,
                     NgayTao = DateTime.Now,
                     NgayCapNhat = DateTime.Now,
@@ -31,22 +29,47 @@ namespace WebAPI.Helpers
                     ThanhTien = thanhTien,
                     TongHoaDon = thanhTien + thanhTien * 5 / 100
                 };
-
-                context.HoaDons.Add(newItem);
-                context.SaveChanges();
-
-
-                var addDetail = AddDetailOrder(IdHoaDon, model.DsChiTietHoaDon, context);
-                if (addDetail)
+                if (string.IsNullOrEmpty(IdNhanVien.ToString()))
                 {
-                    return true;
+                    newItem.IdNhanVien = null;
+                    newItem.TrangThai = false;
+                    context.HoaDons.Add(newItem);
+                    context.SaveChanges();
+
+
+                    var addDetail = AddDetailOrder(IdHoaDon, model.DsChiTietHoaDon, context);
+                    if (addDetail)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        context.HoaDons.Where(x => x.IdHoaDon == IdHoaDon).ExecuteDelete();
+                        context.SaveChanges();
+                        Console.WriteLine("Không thể thêm orderdetail vào db và order đã bị xóa");
+                        return false;
+                    }
                 }
                 else
                 {
-                    context.HoaDons.Where(x => x.IdHoaDon == IdHoaDon).ExecuteDelete();
+                    newItem.IdNhanVien = IdNhanVien;
+                    newItem.TrangThai = true;
+                    context.HoaDons.Add(newItem);
                     context.SaveChanges();
-                    Console.WriteLine("Không thể thêm orderdetail vào db và order đã bị xóa");
-                    return false;
+
+
+                    var addDetail = AddDetailOrder(IdHoaDon, model.DsChiTietHoaDon, context);
+                    if (addDetail)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        context.HoaDons.Where(x => x.IdHoaDon == IdHoaDon).ExecuteDelete();
+                        context.SaveChanges();
+                        Console.WriteLine("Không thể thêm orderdetail vào db và order đã bị xóa");
+                        return false;
+                    }
                 }
             }
             catch (Exception e)
@@ -101,6 +124,61 @@ namespace WebAPI.Helpers
             foreach (var item in ds)
             {
                 Console.WriteLine($"Sản Phẩm: {item.IdSanPham}, Số Lượng: {item.SoLuong}, Giá Bán: {item.GiaBan}, Trạng Thái: {item.TrangThai}");
+            }
+        }
+        public static bool DeleteOrder(Guid IdHoaDon,ApplicationDbContext context) 
+        {
+            try
+            {
+                var item = context.HoaDons.FirstOrDefault(x => x.IdHoaDon == IdHoaDon);
+                if (item == null)
+                {
+                    Console.WriteLine("Không có hóa đơn nào tương tự");
+                    return false;
+                }
+                var ds = context.ChiTietHoaDons.Where(x => x.IdHoaDon == IdHoaDon).ToList();
+                if (ds == null)
+                {
+                    Console.WriteLine($"Không có sản phẩm nào như trên trong chi tiết hóa đơn ");
+                    context.HoaDons.Remove(item);
+                    context.SaveChanges();
+                    return true;
+                }
+                foreach(var chitiet in ds)
+                {
+                    context.ChiTietHoaDons.Remove(chitiet);
+                }
+                    context.SaveChanges();
+                    context.HoaDons.Remove(item);
+                    context.SaveChanges();
+                    return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Lỗi : {e.Message}");
+                return false;
+            }
+        }
+
+        public static bool DeleteOrderDetail(Guid IdHoaDon,Guid IdSanPham, ApplicationDbContext context)
+        {
+            try
+            {
+                var item =context.ChiTietHoaDons.Where(x=>x.IdHoaDon == IdHoaDon&&x.IdSanPham == IdSanPham).FirstOrDefault();
+                if (item == null)
+                {
+                    Console.WriteLine($"Không có sản phẩm nào như trên trong chi tiết hóa đơn ");
+                    return false;
+                }
+                context.ChiTietHoaDons.Remove(item);
+                context.SaveChanges();
+
+                return true;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Lỗi : {e.Message}");
+                return false;
             }
         }
     }
